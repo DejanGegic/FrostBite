7b879baa393db73586ba3e1967df3bf43a991cc4
Dejan Gegic <dejangegic100@gmail.com>
2023-04-02 02:42:26 +0200

    removed need for flags while using admin

--- Admin/main.go
+++ Admin/main.go
@@ -30,36 +30,56 @@ func main() {
 	// decryptedData := DecryptUsingKeyFromFile(encryptedData, err)
 	// os.WriteFile("keys/decrypted.key", []byte(hex.EncodeToString(decryptedData)), 0644)
 
-	var mode []string
+	var mode string
 	// take input from command line of flag --decrypt or --generate
-	if len(os.Args) > 1 {
-		mode = os.Args[1:]
-	}
-	pl("Mode: ", mode)
-	for _, m := range mode {
-		switch m {
-		case "--generate":
-			//confirm if user wants to generate new keys, if yes, then generate
-			generate := ""
-			fmt.Print("Are you sure you want to generate new keys? (y/N)  ")
-			fmt.Scanln(&generate)
-			if generate == "y" {
-				enc.GenerateKeysAndWriteThemToFiles()
-			} else {
-				pl("Exiting...")
-				os.Exit(0)
-			}
-		case "--decrypt":
-			key, err = os.ReadFile("keys/encrypted.key")
-			ErrCheck(err)
-			decrypted := enc.DecryptUsingKeyFromFile()
-			os.WriteFile("keys/decrypted.key", []byte(decrypted), 0644)
-			readFromFileKey, _ := os.ReadFile("keys/decrypted.key")
-			pl("Decrypted key: ", string(readFromFileKey))
+	mode = ChoseMode()
 
-		default:
-			pl("Invalid flag")
+	pl("Mode: ", mode)
+	switch mode {
+	case "generate":
+		//confirm if user wants to generate new keys, if yes, then generate
+		generate := ""
+		fmt.Print("Are you sure you want to generate new keys? (y/N)  ")
+		fmt.Scanln(&generate)
+		if generate == "y" {
+			enc.GenerateKeysAndWriteThemToFiles()
+		} else {
+			pl("Exiting...")
+			os.Exit(0)
+		}
+	case "decrypt":
+		key, err = os.ReadFile("keys/encrypted.key")
+		if err != nil {
+			fmt.Println("Please put encrypted key in keys/encrypted.key. Exiting...")
+			break
+		}
+		decrypted := enc.DecryptUsingKeyFromFile()
+		os.WriteFile("keys/decrypted.key", []byte(decrypted), 0644)
+		readFromFileKey, _ := os.ReadFile("keys/decrypted.key")
+		pl("Decrypted key: ", string(readFromFileKey))
+
+	default:
+		pl("Invalid flag")
+	}
+
+}
+
+func ChoseMode() string {
+	// Mode depends on if "keys" dir exists and if it contains "public.key" and "private.key" files
+	// If "keys" dir exists and contains "public.key" and "private.key" files, then mode is "decrypt"
+	// If "keys" dir exists and does not contain "public.key" and "private.key" files, then mode is "generate"
+	// If "keys" dir does not exist, then mode is "generate"
+	_, err := os.Stat("keys")
+	if os.IsNotExist(err) {
+		return "generate"
+	} else {
+		pl("Keys dir exists")
+		_, errPub := os.Stat("keys/public.key")
+		_, errPriv := os.Stat("keys/private.key")
+		if os.IsNotExist(errPub) || os.IsNotExist(errPriv) {
+			return "generate"
+		} else {
+			return "decrypt"
 		}
 	}
-
 }
